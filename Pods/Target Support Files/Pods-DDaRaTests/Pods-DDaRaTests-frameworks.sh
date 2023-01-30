e33da343eb48fc78985bf4bd1a823bb2839d3308
Pane <kazamajinz@naver.com>
2023-01-30 17:41:59 +0900

    라이브러리 정리

--- Pods/Target Support Files/Pods-DDaRaTests/Pods-DDaRaTests-frameworks.sh
+++ Pods/Target Support Files/Pods-DDaRaTests/Pods-DDaRaTests-frameworks.sh
@@ -180,14 +180,12 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxBlocking/RxBlocking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxTest/RxTest.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Stubber/Stubber.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxBlocking/RxBlocking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxTest/RxTest.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Stubber/Stubber.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
